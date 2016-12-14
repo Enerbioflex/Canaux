98eda338ceaccf87b5e6f6518bbf8d1d122ec5aa
Mougamadou Rizvane <11502358@r10115.iutv.univ-paris13.fr>
2016-12-14 11:41:08 +0100

    Amélioration du forum

---
+++ forum.php
@@ -0,0 +1,39 @@
+<?php
+
+define('DROIT_ACCES', 'membre');
+require('./lib/top.php');
+require './modeles/forum.php';
+
+if (!parametreValide('id'))
+    erreur404();
+
+$forum = getForum($_GET['id']);
+if (!$forum)
+    erreur404();
+
+if (formulaireValide('nom', 'message')) {
+    if (strlen($_POST['nom']) < 2 || strlen($_POST['nom']) > 70)
+        $_SESSION['msgErreur'] = 'Le nom doit contenir entre 2 et 70 caractères';
+    else
+        ajouterTopic($_POST['nom'], $_POST['message'], $_GET['id']);
+}
+
+if (parametreValide('supprId')) {
+    $topic = getTopic($_GET['supprId']);
+    if (!$topic)
+        $_SESSION['msgErreur'] = 'Évitez de traffiquer l\'url svp';
+    elseif ($topic['auteurId'] != $_SESSION['userId'] && !$MODO)
+        $_SESSION['msgErreur'] = 'Évitez de traffiquer l\'url svp';
+    else {
+        supprimerTopic($_GET['supprId']);
+        header('Location: ./forum.php?id=' . $_GET['id']);
+    }
+}
+
+$topics = getTopics($_GET['id']);
+
+securiserTab($forum);
+securiserTab($topics);
+
+define('TITRE', 'Forum ' . $forum['nom']);
+require('./lib/affichage.php');
